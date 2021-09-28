494058147cf2c7a935a04872efd9a67fcf7857ae
MacoveiG <grigore.macovei@isa.utm.md>
2021-09-28 12:25:59 +0300

    schimbare modificatori de acces Main2

--- src/Main2.java
+++ src/Main2.java
@@ -1,21 +1,21 @@
-public class Main2 {
+class Main2 {
     String fName;
     String lName;
     int age;
 
-    public Main2(){
+    Main2(){
         fName = "Macovei";
         lName = "Grigore";
         age = 20;
     }
 
-    public Main2(String fName, String lName, int age){
+    Main2(String fName, String lName, int age){
         this.fName = fName;
         this.lName = lName;
         this.age = age;
     }
 
-    public void hello() {
+    void hello() {
         System.out.println("Hello " + fName + " " + lName);
     }
 
